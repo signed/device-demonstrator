1385f4c3be79de964579f08265fd204eebe78865
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-11-13 21:38:12 +0100

    extend the example

--- src/ReactTestTool.spec.tsx
+++ src/ReactTestTool.spec.tsx
@@ -9,9 +9,19 @@ interface ButtonProps {
 
 const Button = (props: ButtonProps) => <button onClick={props.onClick}>{props.label}</button>;
 
-it('callback should be executed', () => {
-    const nothing = jest.fn();
-    const { getByText } = render(<Button onClick={nothing} label={'hello'}/>);
+it('callback should be executed', async () => {
+    jest.useFakeTimers();
+    const deep = jest.fn();
+    const callback = () => {
+        setTimeout(() => {
+            setTimeout(() => {
+                deep();
+            }, 1000);
+        }, 1000);
+    };
+
+    const { getByText } = render(<Button onClick={callback} label={'hello'}/>);
     fireEvent.click(getByText('hello'));
-    expect(nothing).toHaveBeenCalled()
-});
+    jest.runAllTimers();
+    expect(deep).toHaveBeenCalled();
+}, 500);
