c934218352e790ba6cd194f22df1aa75b3695822
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-12-19 23:01:21 +0100

    move stuff into CameraDemonstrator.tsx

---
+++ src/CameraDemonstrator.tsx
@@ -0,0 +1,48 @@
+import React, { CSSProperties } from 'react';
+import { BigScreen } from './BigScreen';
+import { CameraPicker } from './CameraPicker';
+import { Hide } from './Hide';
+import { Json } from './Json';
+import { Device, RecordingDirector } from './RecordingDirector';
+import { VideoView } from './VideoView';
+
+export interface CameraDemonstratorProps {
+    recordingDirector: RecordingDirector;
+}
+
+export const fetchDevices = (recordingDirector: RecordingDirector) => {
+    navigator.mediaDevices.enumerateDevices()
+        .then(function (mediaDeviceInfos) {
+            const toDevice = (mediaDeviceInfo: MediaDeviceInfo): Device => {
+                return {
+                    groupId: mediaDeviceInfo.groupId,
+                    deviceId: mediaDeviceInfo.deviceId,
+                    label: mediaDeviceInfo.label,
+                    kind: mediaDeviceInfo.kind
+                };
+            };
+            recordingDirector.updateDevices(mediaDeviceInfos.map(toDevice));
+        }).catch(function (err) {
+            console.log(err.name + ': ' + err.message);
+        }
+    );
+};
+
+
+export const CameraDemonstrator: React.FC<CameraDemonstratorProps> = (props) => {
+    const style: CSSProperties = {
+        display: 'flex'
+    };
+    return (
+        <>
+            <div style={style}>
+                <CameraPicker recordingDirector={props.recordingDirector}/>
+                <BigScreen recordingDirector={props.recordingDirector}/>
+            </div>
+            <Hide hide={true}>
+                <VideoView title={'one'}/>
+                <Json content={navigator.mediaDevices.getSupportedConstraints()}/>
+            </Hide>
+        </>
+    );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,60 +1,20 @@
-import React, { CSSProperties } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { CameraDemonstrator, fetchDevices } from './CameraDemonstrator';
 import './index.css';
-import { Hide } from './Hide';
-import { BigScreen } from './BigScreen';
-import { CameraPicker } from './CameraPicker';
-import { Device, RecordingDirector } from './RecordingDirector';
-import { Json } from './Json';
+import { RecordingDirector } from './RecordingDirector';
 import * as serviceWorker from './serviceWorker';
-import { VideoView } from './VideoView';
 
 const recordingDirector = new RecordingDirector();
 
-const fetchDevices = () => {
-    navigator.mediaDevices.enumerateDevices()
-        .then(function (mediaDeviceInfos) {
-            const toDevice = (mediaDeviceInfo: MediaDeviceInfo): Device => {
-                return {
-                    groupId: mediaDeviceInfo.groupId,
-                    deviceId: mediaDeviceInfo.deviceId,
-                    label: mediaDeviceInfo.label,
-                    kind: mediaDeviceInfo.kind
-                };
-            };
-            recordingDirector.updateDevices(mediaDeviceInfos.map(toDevice));
-        }).catch(function (err) {
-            console.log(err.name + ': ' + err.message);
-        }
-    );
-};
-
-export const CameraDemonstrator: React.FC = (props) => {
-    const style: CSSProperties = {
-        display: 'flex'
-    };
-    return (
-        <>
-            <div style={style}>
-                <CameraPicker recordingDirector={recordingDirector}/>
-                <BigScreen recordingDirector={recordingDirector}/>
-            </div>
-            <Hide hide={true}>
-                <VideoView title={'one'}/>
-                <Json content={navigator.mediaDevices.getSupportedConstraints()}/>
-            </Hide>
-        </>
-    );
-};
-
 const renderApplication = () => {
     ReactDOM.render(<div>
-        <CameraDemonstrator/>
+        <CameraDemonstrator recordingDirector={recordingDirector}/>
     </div>, document.getElementById('root'));
 };
 
-fetchDevices();
-navigator.mediaDevices.addEventListener('devicechange', () => fetchDevices());
+fetchDevices(recordingDirector);
+navigator.mediaDevices.addEventListener('devicechange', () => fetchDevices(recordingDirector));
 renderApplication();
 
 // If you want your app to work offline and load faster, you can change
