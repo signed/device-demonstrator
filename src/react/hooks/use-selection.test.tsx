caefabc17f4e731133fc9c8eb734a95fd351ef97
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2020-02-18 22:52:11 +0100

    first test for useSelection

--- src/react/hooks/use-selection.test.tsx
+++ src/react/hooks/use-selection.test.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { select, useSelection } from 'react/hooks/use-selection';
 
-test('hello world with react testing library ', () => {
-    const { getByText } = render(<div>marker</div>);
-    expect(getByText('marker')).toBeInTheDocument();
+const UseSelectionSandbox: React.FC = () => {
+    const [selection, setSelection, textInputRef] = useSelection();
+    return <ul>
+        <li>{`${selection.start}|${selection.end}|${selection.direction}`}</li>
+        <li><input ref={textInputRef} type='text'/></li>
+        <button onClick={() => {
+            textInputRef.current?.focus();
+            setSelection(() => select(1, 2, 'forward'));
+        }}>
+            Quick and Dirty
+        </button>
+    </ul>;
+};
+
+test('get initial selection from input', () => {
+    const { getByText } = render(<UseSelectionSandbox/>);
+    expect(getByText('0|0|none')).toBeInTheDocument();
 });
