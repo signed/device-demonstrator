2afc2d2f3b310f9c1ac927d045cc192eed2ea06a
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2020-02-19 23:13:35 +0100

    prepare formatting spike two with a useFormatter

--- src/input/formatting/formatting-spike-two.tsx
+++ src/input/formatting/formatting-spike-two.tsx
@@ -1,13 +1,28 @@
-import { CountryCode, CountrySelection } from 'input/formatting/shared';
-import React, { CSSProperties, useState } from 'react';
+import { CountryCode, CountrySelection, Formatted, Formatter, ToFormat } from 'input/formatting/shared';
+import { AsYouType } from 'libphonenumber-js/max';
+import { CountryCode as LibCountryCode } from 'libphonenumber-js/types';
+import React, { CSSProperties, useCallback, useState } from 'react';
+import { useSelection } from 'react/hooks/use-selection';
+import { useCallbackRef, useMergeRefs } from 'use-callback-ref';
 
 interface PhoneNumberDigitsProps {
     value: string;
-    onChange: (phoneNumber: string) => void
+    onChange: (phoneNumber: string) => void;
+    formatter: Formatter;
 }
 
-const PhoneNumberDigits: React.FC<PhoneNumberDigitsProps> = ({ value }) => {
-    return <input value={value}/>;
+const useFormatter = (formatter: Formatter) => {
+    const [selection, setSelection, textInputRef] = useSelection();
+    const inputRef = useCallbackRef<HTMLInputElement>(null, (newValue, lastValue) => {
+        console.log(newValue, lastValue);
+    });
+    const combinedInputRef =useMergeRefs([inputRef, textInputRef]);
+    return [combinedInputRef]
+};
+
+const PhoneNumberDigits: React.FC<PhoneNumberDigitsProps> = ({ value, onChange, formatter }) => {
+    const [inputRef] = useFormatter(formatter);
+    return <input ref={inputRef} value={value} onChange={(e) => onChange(e.target.value)}/>;
 };
 
 const PhoneNumberInput: React.FC = () => {
@@ -21,9 +36,24 @@ const PhoneNumberInput: React.FC = () => {
         flexDirection: 'row'
     };
 
+    const phoneNumberFormatter = useCallback<Formatter>((previous: Formatted, toFormat: ToFormat<void>): Formatted => {
+        const asYouType = new AsYouType(countryCode as LibCountryCode);
+        const formatted = asYouType.input(toFormat.value);
+        const caret = toFormat.selection.end === toFormat.value.length ? formatted.length : toFormat.selection.end;
+        return {
+            selection: {
+                start: caret,
+                end: caret,
+                direction: 'forward'
+            },
+            value: formatted
+        };
+    }, [countryCode]);
+
+
     return (<div style={style}>
         <CountrySelection onChange={onCountryCodeChange} value={countryCode}/>
-        <PhoneNumberDigits value={formattedDigits} onChange={onFormattedDigitsChange}/>
+        <PhoneNumberDigits value={formattedDigits} onChange={onFormattedDigitsChange} formatter={phoneNumberFormatter}/>
     </div>);
 };
 
