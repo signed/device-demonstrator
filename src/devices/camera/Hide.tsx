3750be5c694426b4649961af76f5a2495055114a
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2020-06-17 18:41:47 +0200

    convert Hide to functional component

--- src/devices/camera/Hide.tsx
+++ src/devices/camera/Hide.tsx
@@ -4,15 +4,11 @@ export interface HideProps {
     hide: boolean;
 }
 
-export class Hide extends React.Component<HideProps> {
-    render(){
-        if (this.props.hide) {
-            return null;
-        }
-        return (
-            <React.Fragment>
-                {this.props.children}
-            </React.Fragment>
-        );
+export const Hide: React.FC<HideProps> = (props) => {
+    if (props.hide) {
+        return null;
     }
-}
+    return <React.Fragment>
+        {props.children}
+    </React.Fragment>;
+};
