303dae09b5f07195204b029ad879baadaa1f798c
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2020-06-17 20:55:56 +0200

    convert MediaStreamTrackView into function component

--- src/devices/camera/MediaStreamTrackView.tsx
+++ src/devices/camera/MediaStreamTrackView.tsx
@@ -3,36 +3,34 @@ import { Json } from './Json';
 
 export type MediaStreamTrackViewProps = Pick<MediaStreamTrack, 'id' | 'enabled' | 'kind' | 'label' | 'muted' | 'readyState'> & { track: MediaStreamTrack }
 
-export class MediaStreamTrackView extends React.Component<MediaStreamTrackViewProps> {
-    render() {
-        const track = this.props.track;
-        return (
-            <dl>
-                <dt>id</dt>
-                <dd>{this.props.id}</dd>
-                <dt>readyState</dt>
-                <dd>{this.props.readyState}</dd>
-                <dt>enabled</dt>
-                <dd>{String(this.props.enabled)}</dd>
-                <dt>kind</dt>
-                <dd>{this.props.kind}</dd>
-                <dt>label</dt>
-                <dd>{this.props.label}</dd>
-                <dt>muted</dt>
-                <dd>{String(this.props.muted)}</dd>
-                <dt>capabilities</dt>
-                <dd>
-                    <Json content={track.getCapabilities()}/>
-                </dd>
-                <dt>constraints</dt>
-                <dd>
-                    <Json content={track.getConstraints()}/>
-                </dd>
-                <dt>settings</dt>
-                <dd>
-                    <Json content={track.getSettings()}/>
-                </dd>
-            </dl>
-        );
-    }
-}
+export const MediaStreamTrackView: React.FC<MediaStreamTrackViewProps> = (props) => {
+    const { track } = props;
+    return (
+        <dl>
+            <dt>id</dt>
+            <dd>{props.id}</dd>
+            <dt>readyState</dt>
+            <dd>{props.readyState}</dd>
+            <dt>enabled</dt>
+            <dd>{String(props.enabled)}</dd>
+            <dt>kind</dt>
+            <dd>{props.kind}</dd>
+            <dt>label</dt>
+            <dd>{props.label}</dd>
+            <dt>muted</dt>
+            <dd>{String(props.muted)}</dd>
+            <dt>capabilities</dt>
+            <dd>
+                <Json content={track.getCapabilities()}/>
+            </dd>
+            <dt>constraints</dt>
+            <dd>
+                <Json content={track.getConstraints()}/>
+            </dd>
+            <dt>settings</dt>
+            <dd>
+                <Json content={track.getSettings()}/>
+            </dd>
+        </dl>
+    );
+};
