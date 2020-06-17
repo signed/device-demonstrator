7afd7f38d845a0d19cabcb8439c90e92235d3446
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2020-06-17 21:11:51 +0200

    convert VideoView into function component

--- src/devices/camera/VideoView.tsx
+++ src/devices/camera/VideoView.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties } from 'react';
+import React, { CSSProperties, useState } from 'react';
 import { MediaStreamTrackView } from './MediaStreamTrackView';
 import { VideoElement } from './VideoElement';
 
@@ -6,92 +6,78 @@ export interface VideoViewProps {
     title: string;
 }
 
-interface VideoViewState {
-    source: MediaStream | null;
-}
+export const VideoView: React.FC<VideoViewProps> = (props) => {
+    const { title } = props;
+    const [source, setSource] = useState<MediaStream | null>(null);
 
-export class VideoView extends React.Component<VideoViewProps, VideoViewState> {
-
-    constructor(props: VideoViewProps) {
-        super(props);
-        this.state = { source: null };
-    }
-
-    private handleStart = () => {
+    const handleStart = () => {
         navigator.mediaDevices.getUserMedia({ video: true })
-            .then((stream: MediaStream) => {
-                this.setState({
-                    source: stream
-                });
-            }).catch((err: Error) => {
-            alert(err);
-        });
+            .then((stream: MediaStream) => setSource(() => stream))
+            .catch((err: Error) => alert(err));
     };
 
-    private handlePause = () => {
-        this.tracks().forEach(track => track.enabled = false);
-        this.triggerRender();
+    const triggerRender = () => {
+        setSource(prev => prev);
     };
 
-    private handleContinue = () => {
-        this.tracks().forEach(track => track.enabled = true);
-        this.triggerRender();
-    };
-
-    private handleStop = () => {
-        this.tracks().forEach(track => track.stop());
-        this.triggerRender();
-    };
-
-    private tracks(): Array<MediaStreamTrack> {
-        const maybeStream = this.state.source;
+    const tracks = (): Array<MediaStreamTrack> => {
+        const maybeStream = source;
         if (null === maybeStream) {
             return [];
         }
         return maybeStream.getTracks();
-    }
-
-    private handleDetach = () => {
-        this.setState({ source: null });
     };
 
-    render() {
-        const topStyles: CSSProperties = {
-            display: 'flex'
-        };
+    const handlePause = () => {
+        tracks().forEach(track => track.enabled = false);
+        triggerRender();
+    };
 
-        const controlStyles: CSSProperties = {
-            display: 'flex',
-            flexDirection: 'column'
-        };
+    const handleContinue = () => {
+        tracks().forEach(track => track.enabled = true);
+        triggerRender();
+    };
 
-        const tracks = this.tracks().map(track => {
-            return (<li key={track.id}>
-                <MediaStreamTrackView enabled={track.enabled} id={track.id} kind={track.kind} label={track.label} muted={track.muted} readyState={track.readyState} track={track}/>
-            </li>);
-        });
-        return (
-            <div style={topStyles}>
-                <h1>{this.props.title}</h1>
-                <div style={controlStyles}>
-                    <button onClick={this.handleStart}>start</button>
-                    <button onClick={this.handlePause}>pause</button>
-                    <button onClick={this.handleContinue}>continue</button>
-                    <button onClick={this.handleStop}>stop</button>
-                    <button onClick={this.handleDetach}>detach</button>
-                </div>
-                <VideoElement id="video-chat" srcObject={this.state.source} autoPlay={true}/>
-                <div>
-                    <h2>tracks</h2>
-                    <ul>
-                        {tracks}
-                    </ul>
-                </div>
+    const handleStop = () => {
+        tracks().forEach(track => track.stop());
+        triggerRender();
+    };
+
+    const handleDetach = () => {
+        setSource(null);
+    };
+
+    const topStyles: CSSProperties = {
+        display: 'flex'
+    };
+
+    const controlStyles: CSSProperties = {
+        display: 'flex',
+        flexDirection: 'column'
+    };
+
+    const tracks1 = tracks().map(track => {
+        return (<li key={track.id}>
+            <MediaStreamTrackView enabled={track.enabled} id={track.id} kind={track.kind} label={track.label} muted={track.muted} readyState={track.readyState} track={track}/>
+        </li>);
+    });
+    return (
+        <div style={topStyles}>
+            <h1>{title}</h1>
+            <div style={controlStyles}>
+                <button onClick={handleStart}>start</button>
+                <button onClick={handlePause}>pause</button>
+                <button onClick={handleContinue}>continue</button>
+                <button onClick={handleStop}>stop</button>
+                <button onClick={handleDetach}>detach</button>
             </div>
-        );
-    }
-
-    private triggerRender() {
-        this.setState(this.state);
-    }
-}
+            <VideoElement id="video-chat" srcObject={source} autoPlay={true}/>
+            <div>
+                <h2>tracks</h2>
+                <ul>
+                    {tracks1}
+                </ul>
+            </div>
+        </div>
+    );
+};
