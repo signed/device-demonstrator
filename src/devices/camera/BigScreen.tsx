9ee29ea95381e9f7755be0558f7733e293755cef
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2020-06-17 18:39:19 +0200

    not sure why the hook implementation results in an infinite loop

--- src/devices/camera/BigScreen.tsx
+++ src/devices/camera/BigScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Device, MediaStreamSubscription, RecordingDirector } from './RecordingDirector';
 import { VideoElement } from './VideoElement';
 
@@ -13,7 +13,73 @@ export interface BigScreenState {
     streamError: boolean;
 }
 
-export class BigScreen extends React.Component<BigScreenProps, BigScreenState> {
+export const BigScreen: React.FC<BigScreenProps> = (props) => {
+    console.log('BigScreen called');
+    const { recordingDirector } = props;
+    const [state, setState] = useState<BigScreenState>({ streamError: false, stream: null, mediaStreamSubscription: undefined, device: undefined });
+    const { mediaStreamSubscription } = state;
+
+    console.log('mss: ' + mediaStreamSubscription)
+
+    const closeExistingStream = useCallback(() => {
+        if (mediaStreamSubscription !== undefined) {
+            mediaStreamSubscription.cancel();
+            setState((prev) => ({ ...prev, stream: null, mediaStreamSubscription: undefined }));
+        }
+    }, [mediaStreamSubscription]);
+
+    useEffect(() => {
+        const handleDeviceSelectionChange = (device: Device | void): void => {
+            closeExistingStream();
+            setState((prev => {
+                return { ...prev, device };
+            }));
+            if (device === undefined) {
+                return;
+            }
+            const mediaStreamSubscription = recordingDirector.videoStreamSubscriptionFor(device);
+            setState(prev => {
+                return { ...prev, mediaStreamSubscription };
+            });
+            mediaStreamSubscription.stream
+                .then(stream => setState(prev => ({ ...prev, stream })))
+                .catch(() => setState(prev => ({ ...prev, streamError: true })));
+        };
+        recordingDirector.addOnCameraSelectionChanged(handleDeviceSelectionChange);
+        return () => {
+            recordingDirector.removeOnCameraSelectionChanged(handleDeviceSelectionChange);
+            console.log('removed OnCameraSelectionChanged listener');
+        };
+    }, [recordingDirector, closeExistingStream]);
+
+    useEffect(() => {
+        console.log('empty setup')
+        return () => {
+            console.log('tear down no unmount at least that is the intend')
+            closeExistingStream();
+            setState(prev => ({ ...prev, device: undefined, streamError: false }));
+        };
+    }, [closeExistingStream]);
+
+    if (state.device === undefined) {
+        return <div>No device selected</div>;
+    }
+    if (state.stream === null) {
+        return <div>Opening stream</div>;
+    }
+
+    const handleVideoClicked = () => {
+        props.recordingDirector.clearCameraSelection();
+    };
+    return (
+        <div>
+            {<VideoElement srcObject={state.stream} autoPlay={true} onClick={handleVideoClicked}/>}
+            <div>{state.stream.id}</div>
+        </div>
+    );
+};
+
+export class BigScreenOld extends React.Component<BigScreenProps, BigScreenState> {
 
     constructor(props: BigScreenProps) {
         super(props);
@@ -48,7 +114,7 @@ export class BigScreen extends React.Component<BigScreenProps, BigScreenState> {
 
     private handleVideoClicked = () => {
         this.props.recordingDirector.clearCameraSelection();
-    }
+    };
 
     private handleDeviceSelectionChange = (device: Device | void): void => {
         this.closeExistingStream();
