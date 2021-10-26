2ad6f8ff558bcf47eca8105569d503b0110023e1
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2021-10-26 20:57:14 +0200

    error boundry example

---
+++ src/react/error-boundry/error-boundry.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ErrorBoundryExample } from './error-boundry';
+
+export default {
+    title: 'ErrorBoundry',
+    component: ErrorBoundryExample
+};
+
+export const Basic = () => (
+    <ErrorBoundryExample/>
+);
